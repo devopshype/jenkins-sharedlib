08bd98a751d760061a3c2cc2cd8e03ea9edfd6f2
Anil Aithal <152262171+devopshype@users.noreply.github.com>
2024-02-10 13:59:37 +0530

    Create dockerECRImagePush.groovy

---
+++ vars/dockerECRImagePush.groovy
@@ -0,0 +1,43 @@
+def call(String dockerRegistry, String dockerImageTag, String ecrRepo, String awsCredID, String awsRegion){
+    // sh """
+    //       if ! command -v aws > /dev/null; then
+    //       echo "AWS CLI not found. Installing AWS CLI..."
+    //       curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 2> /dev/null
+            
+    //         dpkg-query -l unzip > /dev/null || sudo apt-get update && sudo apt-get -y install unzip
+            
+    //         unzip awscliv2.zip > /dev/null
+    //         sudo ./aws/install > /dev/null
+    //         rm -rf awscliv2.zip aws
+    //         echo "AWS CLI installed successfully."
+    //     fi
+    // """
+
+    withCredentials([usernamePassword(
+        credentialsId: "$awsCredID",
+        usernameVariable: "awsAccessKey",
+        passwordVariable: "awsSecretKey"
+    )]) {
+        sh """
+            # Configure AWS CLI
+            aws configure set aws_access_key_id $awsAccessKey
+            aws configure set aws_secret_access_key $awsSecretKey
+            aws configure set region $awsRegion
+
+            # Check if the repository exists and create it if it does not
+            if ! aws ecr describe-repositories --repository-names $ecrRepo --region $awsRegion >/dev/null 2>&1; then
+                echo "Repository $ecrRepo does not exist. Creating repository."
+                aws ecr create-repository --repository-name $ecrRepo --region $awsRegion
+            else
+                echo "Repository $ecrRepo already exists."
+            fi
+
+            # Login to ECR registry
+            aws ecr get-login-password --region $awsRegion | docker login --username AWS --password-stdin $ecrRepo
+
+            # Push the Docker images
+            docker image push $ecrRepo:$dockerImageTag
+            docker image push $ecrRepo:latest
+        """
+    }
+}
