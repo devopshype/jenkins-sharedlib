47b806628a0ed8a3ced3aeb93d2be98b8d26159a
Anil Aithal <152262171+devopshype@users.noreply.github.com>
2024-04-11 20:19:15 +0530

    Update dockerECRImagePush.groovy

--- vars/dockerECRImagePush.groovy
+++ vars/dockerECRImagePush.groovy
@@ -1,17 +1,17 @@
 def call(String dockerRegistry, String dockerImageTag, String ecrRepo, String awsCredID, String awsRegion){
-    // sh """
-    //       if ! command -v aws > /dev/null; then
-    //       echo "AWS CLI not found. Installing AWS CLI..."
-    //       curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 2> /dev/null
+    sh """
+        if ! command -v aws > /dev/null; then
+            echo "AWS CLI not found. Installing AWS CLI..."
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 2> /dev/null
             
-    //         dpkg-query -l unzip > /dev/null || sudo apt-get update && sudo apt-get -y install unzip
+            dpkg-query -l unzip > /dev/null || sudo apt-get update && sudo apt-get -y install unzip
             
-    //         unzip awscliv2.zip > /dev/null
-    //         sudo ./aws/install > /dev/null
-    //         rm -rf awscliv2.zip aws
-    //         echo "AWS CLI installed successfully."
-    //     fi
-    // """
+            unzip awscliv2.zip > /dev/null
+            sudo ./aws/install > /dev/null
+            rm -rf awscliv2.zip aws
+            echo "AWS CLI installed successfully."
+        fi
+    """
 
     withCredentials([usernamePassword(
         credentialsId: "$awsCredID",
@@ -32,12 +32,12 @@ def call(String dockerRegistry, String dockerImageTag, String ecrRepo, String aw
                 echo "Repository $ecrRepo already exists."
             fi
 
-            # Login to ECR registry
-            aws ecr get-login-password --region $awsRegion | docker login --username AWS --password-stdin $ecrRepo
+            # Login to Docker registry
+            aws ecr get-login-password --region $awsRegion | docker login --username AWS --password-stdin $dockerRegistry
 
             # Push the Docker images
-            docker image push $ecrRepo:$dockerImageTag
-            docker image push $ecrRepo:latest
+            docker image push $dockerRegistry:$dockerImageTag
+            docker image push $dockerRegistry:latest
         """
     }
 }
